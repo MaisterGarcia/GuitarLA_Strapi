6ccd4bfb5a5017365ce9afa7df2efd6b1d90842d
MaisterGarcia <jorge_08emmanuel@hotmail.com>
2022-07-27 08:45:46 -0500

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4043a1cb96b56abd6e959c92586b6fb7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5a6addf2-09c6-4a53-88d9-0ab62dbefb5e'
+};
